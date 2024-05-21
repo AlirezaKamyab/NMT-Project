57a432568a6ece720b603494da64ba8622077760
Alireza Kamyab <alirezakamyab19@gmail.com>
2024-05-21 18:35:54 +0330

    Changes the algorithm
    This script now takes a file with sentences separted via \n, then translates
    batches of sentences.
    
    Multiple batches are translated each on a single thread

--- data_cleaner/take_or_leave.py
+++ data_cleaner/take_or_leave.py
@@ -1,124 +1,110 @@
 #!/usr/bin/env python3
 import os
-import sys
-from parsivar import Normalizer
-import nltk
-from deep_translator import GoogleTranslator
 import time
+
+from deep_translator import GoogleTranslator
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+BATCH_SIZE = 20
+MAX_THREADS = 5
 
 
 def main():
-    args = sys.argv[1:]
     try:
         translator = GoogleTranslator(source='en', target='fa')
     except:
         translator = None
 
-    if len(args) > 0:
-        filename = args[0]
-    else:
-        filename = input('filename >> ')
+    filename = input('filename >> ')
+    sep = input('SEP >> ')
 
-    if len(args) > 1:
-        sep = args[1]
-    else:
-        sep = input('SEP >> ')
+    if translator is None:
+        print('Could not load the translator !!!')
+        return
 
-    if translator is not None:
-        auto = input('Do you want to clean in automated mode? (Y/N)')
-    else: auto = False
-    if auto.lower() == 'y':
-        auto = True
-        new_filename = input('Filename to save to >> ')
-        rest_filename = input('Save the undone (blank to not to save) >> ')
-    else: auto = False
+    new_filename = input('Filename to save to >> ')
+    rest_filename = input('Save the undone (blank to use the original file) >> ')
+
+    if rest_filename.strip() == '':
+        rest_filename = filename
 
     with open(filename, 'r') as file:
         texts = file.read().split('\n')
 
-    normalizer = Normalizer()
-    tokenizer = nltk.tokenize.NLTKWordTokenizer()
-    taken = []
-
+    total_batches = []
     i = 0
-    while i < len(texts):
-        if texts[i].strip() == '':
+    for batch in range(len(texts) // BATCH_SIZE):
+        batch_list = []
+        while len(batch_list) != BATCH_SIZE:
+            batch_list.append(texts[i].split(sep=sep)[0])
             i += 1
-            continue
+        total_batches.append(batch_list)
 
-        text = texts[i].split(sep)
-        en = ' '.join(tokenizer.tokenize(text[0]))
-        fa = normalizer.normalize(text[1]).replace('\u200c', ' ')
+    batch_count = len(total_batches)
 
-        print(f'\n\n{i + 1:<5} out of {len(texts):<5} : {len(taken):<5} Taken')
-        print('SOURCE:', f'\t{en}')
-        print('TARGET:', f'\t{fa}')
-
-        if len(en) < 3 or len(fa) < 3:
-            i += 1
-            print('Invalid input, skipping . . .')
-            continue
-        if translator is not None:
-            while True:
-                try:
-                    google = translator.translate(text[0])
-                    break
-                except:
-                    print('Establishing connection . . .')
-
-            if google is None:
-                i += 1
-                continue
-
-            google = ' '.join(tokenizer.tokenize(google))
-
-        if translator is not None:
-            print('GOOGLE TRANSLATION:', f'\t{google}\n')
-        else: print()
-
-        if not auto:
-            inp = input('ENTER to take\nG take google\nL to leave\nB to go back\nE to exit and save >> ')
-        else:
-            inp = 'g'
-            print()
-
-        if inp.strip() == '':
+    taken = []
+    batches_done = []
+    checkpoint = 0
+    cnt = 0
+    for en_batch, fa_batch, batch_id in batch_executor(total_batches, translator):
+        print(f'\rBatch {cnt + 1:>6} out of {batch_count:>6}', end='')
+        for i in range(len(en_batch)):
+            en, fa = en_batch[i], fa_batch[i]
+            if len(en.strip()) < 3: continue
             taken.append(sep.join([en, fa]))
-        if inp.strip().lower() == 'g':
-            taken.append(sep.join([en, google]))
+            batches_done.append(batch_id)
 
-        elif inp.lower() == 'b':
-            taken.pop()
-            i = max(i - 1, 0)
-            continue
-        elif inp.lower() == 'e':
-            break
+        if cnt >= checkpoint:
+            save(new_filename, rest_filename, taken, total_batches, batches_done)
+            checkpoint = cnt + (0.01 * len(total_batches) // BATCH_SIZE)
+            print('\nCheckpoint')
 
-        i += 1
+        cnt += 1
 
-        if i % 100 == 0:
-            save(new_filename, rest_filename, taken, texts, i)
-            print('Checkpoint')
-
-    if not auto:
-        new_filename = input('Filename >> ')
-        rest_filename = input('Save the undone (blank to not to save) >> ')
-    save(new_filename, rest_filename, taken, texts, i)
-
+    save(new_filename, rest_filename, taken, total_batches, batches_done)
     return
 
 
-def save(new_filename, rest_filename, taken, texts, i):
+def translate_batch(batch, translator, batch_id=0):
+    while True:
+        try:
+            google = translator.translate_batch(batch)
+            return batch, google, batch_id
+        except:
+            print('Establishing connection . . .')
+            time.sleep(1)
+
+
+def batch_executor(batches, translator):
+    with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
+        futures = [executor.submit(translate_batch,
+                                   batch=batches[i],
+                                   translator=translator,
+                                   batch_id=i)
+                   for i in range(len(batches))]
+
+        for future in as_completed(futures):
+            try:
+                yield future.result()
+            except:
+                pass
+
+
+def save(new_filename, rest_filename, taken, total_batches, batches_done):
     if new_filename.strip() == '':
         new_filename = 'output.txt'
     with open(new_filename, 'w') as file:
         file.write('\n'.join(taken))
 
-    if i < len(texts):
-        if rest_filename.strip() == '': return
+    if len(batches_done) < len(total_batches):
+        if rest_filename.strip() == '':
+            return
         with open(rest_filename, 'w') as file:
-            file.write('\n'.join(texts[i:]))
-    elif i >= len(texts):
+            for batch_id in range(len(total_batches)):
+                if batch_id in batches_done: continue
+                for data in total_batches[batch_id]:
+                    file.write(data + '\n')
+    else:
         os.remove(rest_filename)
 
 
