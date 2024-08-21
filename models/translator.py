04fe9b63d5cf6c8b1885d2da4333dd93ca86c021
Alireza Kamyab <alirezakamyab19@gmail.com>
2024-08-21 21:14:28 +0330

    Torch Implementation

---
+++ models/translator.py
@@ -0,0 +1,38 @@
+import torch
+
+class GreedyTranslator(torch.nn.Module):
+    def __init__(self, transformer, en_tokenizer, fa_tokenizer, max_length=4000):
+        super(GreedyTranslator, self).__init__()
+
+        transformer.eval()
+        self.encoder = transformer.encoder_layer
+        self.decoder = transformer.decoder_layer
+        self.classifier = transformer.classifier
+        self.en_tokenizer = en_tokenizer
+        self.fa_tokenizer = fa_tokenizer
+        self.cls = fa_tokenizer.token_to_id('[CLS]')
+        self.sep = fa_tokenizer.token_to_id('[SEP]')
+        self.pad = fa_tokenizer.token_to_id('[PAD]')
+        self.max_length = max_length
+
+
+    def forward(self, source:list):
+        device = next(self.parameters()).device
+        source = torch.tensor([x.ids for x in self.en_tokenizer.encode_batch(source)], dtype=torch.int32, device=device)
+        target = torch.tensor([[self.cls]] * source.shape[0], dtype=torch.int32, device=device)
+        pending = torch.tensor([1] * source.shape[0], dtype=torch.int32, device=device)
+        
+        with torch.no_grad():
+            context = self.encoder(source)
+
+            for _ in range(self.max_length):
+                outputs = self.decoder(target, context)
+                outputs = self.classifier(outputs)
+                outputs = torch.argmax(outputs, dim=-1)[:, -1]
+                outputs = outputs.unsqueeze(dim=1) * pending + (1 - pending) * self.pad
+                pending = torch.logical_not(torch.eq(outputs[:, 0], self.sep)).to(dtype=torch.int32) * pending
+                target = torch.concat([target, outputs], dim=-1)
+
+                if torch.sum(pending) == 0:
+                    break
+        return self.fa_tokenizer.decode_batch(target.cpu().numpy(), skip_special_tokens=True)
---
+++ trainer.py
@@ -0,0 +1,202 @@
+import os
+os.environ['TOKENIZERS_PARALLELISM'] = 'true'
+import sys
+import torch
+import time
+from tokenizers import Tokenizer
+from torch.utils.tensorboard import SummaryWriter
+from data.dataloader import load_data, get_pipelines
+from models.transformer.transformer import Transformer
+from torch.optim import Adam
+import json
+
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+
+
+def main():
+    config_file = sys.argv[1]
+    with open(config_file, 'r') as file:
+        config = json.load(file)
+
+    dataset_path = config['files']['dataset']
+    en_tokenizer_path = config['files']['english_tokenizer']
+    fa_tokenizer_path = config['files']['persian_tokenizer']
+
+    batch_size = config['pipeline']['batch_size']
+    num_workers = config['pipeline']['num_workers']
+    prefetch_factor = config['pipeline']['prefetch_factor']
+    pin_memory = config['pipeline']['pin_memory']
+    seed = config['pipeline']['seed']
+
+    d_model = config['model']['d_model']
+    num_heads = config['model']['num_heads']
+    N = config['model']['N']
+    dff = config['model']['dff']
+    dropout_rate = config['model']['dropout_rate']
+    label_smoothing = config['model']['label_smoothing']
+
+    beta1 = config['optimizer']['beta1']
+    beta2 = config['optimizer']['beta2']
+    epsilon = config['optimizer']['epsilon']
+    warmup_steps = config['optimizer']['warmup_steps']
+    epochs = config['optimizer']['epochs']
+
+    torch.manual_seed(seed)
+    en_tokenizer = Tokenizer.from_file(en_tokenizer_path)
+    fa_tokenizer = Tokenizer.from_file(fa_tokenizer_path)
+    en_pad_id = en_tokenizer.padding['pad_id']
+    fa_pad_id = fa_tokenizer.padding['pad_id']
+
+    (en_tokenized_train, fa_tokenized_train), (en_tokenized_test, fa_tokenized_test) = load_data(dataset_path, 
+                                                                                                return_tokenized=True, 
+                                                                                                en_tokenizer=en_tokenizer, 
+                                                                                                fa_tokenizer=fa_tokenizer)
+
+    trainloader, testloader = get_pipelines(en_tokenized_train, 
+                                            fa_tokenized_train,
+                                            en_tokenized_test,
+                                            fa_tokenized_test, 
+                                            batch_size=batch_size, 
+                                            num_workers=num_workers, 
+                                            prefetch_factor=prefetch_factor, 
+                                            pin_memory=pin_memory,
+                                            en_pad_id=en_pad_id, 
+                                            fa_pad_id=fa_pad_id)
+
+
+    source_vocab_size = en_tokenizer.get_vocab_size()
+    target_vocab_size = fa_tokenizer.get_vocab_size()
+
+    transformer = Transformer(d_model=d_model, 
+                            num_heads=num_heads, 
+                            N=N, 
+                            dff=dff, 
+                            dropout=dropout_rate,
+                            source_vocab_size=source_vocab_size, 
+                            target_vocab_size=target_vocab_size, 
+                            source_padding_idx=en_pad_id, 
+                            target_padding_idx=fa_pad_id)
+    transformer = transformer.to(device)
+
+
+    optimizer = Adam(params=transformer.parameters(), betas=[beta1, beta2], eps=epsilon)
+    loss_func = torch.nn.CrossEntropyLoss(label_smoothing=label_smoothing, reduction='none')
+    criterion = get_criterion(loss_func=loss_func, target_vocab_size=target_vocab_size)
+    scheduler = get_scheduler(d_model=torch.tensor(d_model), warmup_steps=torch.tensor(warmup_steps))
+
+
+    writer= SummaryWriter(log_dir='./logs')
+
+    total_steps = 0
+    for epoch in range(epochs):
+        running_loss = 0.0
+        running_acc = 0.0
+        loss_metric = {}
+        accuracy_metric = {}
+        start = time.time()
+        print(f'EPOCH {epoch + 1}')
+        print('-----------------------------')
+        transformer.train()
+        for step, (source, target) in enumerate(trainloader):
+            learning_rate = scheduler(total_steps)
+            change_learning_rate(optimizer, learning_rate)
+            writer.add_scalar('learning rate', learning_rate, global_step=total_steps)
+            
+            optimizer.zero_grad()
+            source = source.to(device)
+            target = target.to(device)
+            y_true = target[:, 1:]
+            target = target[:, :-1]
+
+            y_hat = transformer(source, target)
+            loss = criterion(y_hat, y_true.to(dtype=torch.long))
+            loss.backward()
+            optimizer.step()
+
+            acc = masked_accuracy(y_hat, y_true, fa_pad_id)
+            end = time.time()
+            
+            running_loss = running_loss + (1 / (step + 1)) * (loss - running_loss)
+            running_acc = running_acc + (1 / (step + 1)) * (acc - running_acc)
+            print(f'\r{int(end - start):>4} sec | Step {step:>5}\t Loss {running_loss:>2.4f}\t Accuracy {running_acc:>0.3f}', end='')
+            total_steps += 1
+        
+        print()
+        loss_metric['train'] = running_loss
+        accuracy_metric['train'] = running_acc
+        print('Validation')
+        transformer.eval()
+        running_loss = 0.0
+        running_acc = 0.0
+
+        with torch.no_grad():
+
+            for step, (source, target) in enumerate(testloader):
+                source = source.to(device)
+                target = target.to(device)
+                y_true = target[:, 1:]
+                target = target[:, :-1]
+        
+                y_hat = transformer(source, target)
+                loss = criterion(y_hat, y_true.to(dtype=torch.long))    
+                acc = masked_accuracy(y_hat, y_true, fa_pad_id)
+                end = time.time()
+                
+                running_loss = running_loss + (1 / (step + 1)) * (loss - running_loss)
+                running_acc = running_acc + (1 / (step + 1)) * (acc - running_acc)
+                print(f'\r{int(end - start):>4} sec | Step {step:>5}\t Loss {running_loss:>2.4f}\t Accuracy {running_acc:>0.3f}', end='')
+
+            loss_metric['validation'] = running_loss
+            accuracy_metric['validation'] = running_acc
+
+            writer.add_scalars('loss', loss_metric, global_step=total_steps)
+            writer.add_scalars('accuracy', accuracy_metric, global_step=total_steps)
+
+            os.makedirs('./checkpoints', exist_ok=True)
+            torch.save(transformer.state_dict(), f'./checkpoints/epoch_{epoch + 1}.pt')
+            print(f'\n')
+    
+    torch.save(transformer, 'model.pt')
+
+
+def change_learning_rate(optimizer, lr):
+    for param in optimizer.param_groups:
+        param['lr'] = lr
+        
+
+def masked_accuracy(y_hat, y_true, mask_id):
+    mask = torch.logical_not(y_true == mask_id)
+    mask = mask.to(dtype=torch.float32)
+    y_pred = torch.argmax(y_hat, dim=-1)
+    correct = torch.eq(y_true, y_pred)
+    correct = correct.to(dtype=torch.float32) * mask
+    return torch.sum(correct) / torch.sum(mask)
+
+
+def get_scheduler(d_model, warmup_steps=4000):
+    def get_learning_rate(step):
+        step = torch.tensor(step, dtype=torch.float32)
+        arg1 = torch.rsqrt(step)
+        arg2 = step * torch.pow(warmup_steps * 1.0, -1.5)
+
+        return torch.rsqrt(d_model * 1.0) * torch.minimum(arg1, arg2)
+
+    return get_learning_rate
+
+
+def get_criterion(loss_func, target_vocab_size):
+    def criterion(y_hat, y_true):
+        y_hat = torch.reshape(y_hat, [-1, target_vocab_size])
+        y_true = torch.reshape(y_true, [-1])
+        mask = torch.logical_not(y_true == 0)
+        mask = mask.to(dtype=torch.float32)
+        loss = loss_func(y_hat, y_true) * mask
+        return torch.sum(loss) / torch.sum(mask)
+    return criterion
+
+
+if __name__ == '__main__':
+    main()
